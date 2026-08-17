1d69f9625a7a7de8424571cab0ad4484fbb1076c
OrAzrad4 <orazrad4@gmail.com>
2026-08-17 20:11:02 +0300

    SPSC lock-free

---
+++ build/CMakeFiles/packet_engine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/packet_engine.dir/src/packet_pool.c.o"
+  "CMakeFiles/packet_engine.dir/src/packet_pool.c.o.d"
+  "CMakeFiles/packet_engine.dir/src/ring_buffer.c.o"
+  "CMakeFiles/packet_engine.dir/src/ring_buffer.c.o.d"
+  "libpacket_engine.a"
+  "libpacket_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/packet_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/pipeline_app.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pipeline_app.dir/src/main.c.o"
+  "CMakeFiles/pipeline_app.dir/src/main.c.o.d"
+  "pipeline_app"
+  "pipeline_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pipeline_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
